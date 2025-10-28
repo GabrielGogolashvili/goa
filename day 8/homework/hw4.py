e11cd0d7c948559a5780484044d223a4714799d0
GabrielGogolashvili <gogolashviligabriel9@gmail.com>
2025-10-28 19:35:45 +0200

    commit

--- day 8/homework/hw4.py
+++ day 8/homework/hw4.py
@@ -5,5 +5,7 @@ if age<12 or age>65:
     print("ticket is on the house")
 elif student=="yes" and age>12:
     print("ticket is half the price")
+elif student!="yes" or student!="no":
+    print("answer with yes or no")
 else:
     print("pay the full price")
