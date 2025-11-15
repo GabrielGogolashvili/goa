ebf8fa8b0bdb1e6e3eddde950320f5301e48fd32
GabrielGogolashvili <gogolashviligabriel9@gmail.com>
2025-11-15 16:01:11 +0200

    commit

--- day 14/homework/hw6.py
+++ day 14/homework/hw6.py
@@ -1,4 +1,11 @@
-number = int(input("Enter your number: "))
+number = (input("Enter your number or გამოთვალე შაშუალო: "))
+sum_numbers = 0
+count_numbers = 0
 
 while number != "გამოთვალე საშუალო":
-    number = int(input("Enter your number: "))
+    count_numbers += 1
+    sum_numbers = sum_numbers + int(number)
+    number = (input("Enter your number or გამოთვალე შაშუალო: "))
+
+sashualo = sum_numbers/count_numbers
+print(sashualo)
