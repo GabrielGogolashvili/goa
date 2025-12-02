fe39a46a264a9cbdc5f09ecd31245cdf756a489f
GabrielGogolashvili <gogolashviligabriel9@gmail.com>
2025-12-02 19:51:50 +0200

    commit

--- day 18/homework/hw3.py
+++ day 18/homework/hw3.py
@@ -14,7 +14,7 @@ num = int(input("Enter your number: "))
 if num > 0:
     if num % 2 == 0:
         print("+ even")
-    if num % 2 == 1:
+    elif num % 2 == 1:
         print("+ odd")
 else:
     print("Not more than 0")
